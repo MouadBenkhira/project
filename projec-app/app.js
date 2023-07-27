6a531a65910d5fa8d4abdb1615254978c2d583d6
MouadBenkhira <140723377+MouadBenkhira@users.noreply.github.com>
2023-07-27 15:50:38 +0200

    Update app.js

--- projec-app/app.js
+++ projec-app/app.js
@@ -1,1 +1,42 @@
-.
+import React, { useEffect, useState } from 'react';
+import './styles.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import MainPage from './components/MainPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
+const App = () => {
+  const [groups, setGroups] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchGroups();
+  }, []);
+
+  const fetchGroups = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/api/v1/Worker'); // Make sure to update the endpoint URL according to your backend API
+      setGroups(response.data);
+      setLoading(false);
+      console.log(response.data);
+    } catch (err) {
+      console.error('Error fetching data:', err);
+      setLoading(false);
+    }
+  };
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      <Navbar />
+      <MainPage groups={groups} />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
